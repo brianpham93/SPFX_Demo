c0430e2b1ad4eb1a34b99f3e5adcfeda4aceede6
Phạm Quốc Thắng <thangpq@outlook.com.vn>
2017-01-07 10:54:46 +0700

    Rename ContactsWebParts.ts -> tsx

---
+++ src/webparts/contacts/ContactsWebPart.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import {Router, Route, browserHistory,IndexRoute} from "react-router"
+import {Provider} from "react-redux"
+import * as createLogger from "redux-logger"
+import {createStore, applyMiddleware} from 'redux';
+import {ContactList, SearchScreen} from './components';
+
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneSettings,
+  IWebPartContext,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'contactsStrings';
+import {AlphabetScreen} from './components';
+import { IContactsWebPartProps } from './IContactsWebPartProps';
+import {rootReducer} from "./reducers"
+import {DumpContactList, AlphabetChars} from "./constants"
+import {IGlobalState, IProviderProps} from './interfaces';
+
+let initalState:IGlobalState = {
+	contacts: DumpContactList,
+	alphabetChars: AlphabetChars
+}
+
+let logger = createLogger()
+let createStoreWithMiddleware = applyMiddleware(logger)(createStore)
+let store = createStoreWithMiddleware(rootReducer, initalState)
+
+export default class ContactsWebPart extends BaseClientSideWebPart<IContactsWebPartProps> {
+
+  public constructor(context: IWebPartContext) {
+    super(context);
+  }
+
+  public render(){
+    return (
+      <Provider store={store}>
+        <Router history={browserHistory}>
+              <Route path="/" component={AlphabetScreen}>                                
+                  <IndexRoute component={AlphabetScreen}/>
+              </Route>            
+              <Route>                
+                  <IndexRoute component={SearchScreen}/>
+                  <Route path="contacts/(:char)" component={SearchScreen}/>
+              </Route>
+          </Router>    
+      </Provider>
+    )
+  }
+  // public render(): void {    
+  //   const element: React.ReactElement<IProviderProps> = React.createElement(Provider, {
+  //     store
+  //   });
+
+  //   ReactDom.render(element, this.domElement);
+  // }
+
+  protected get propertyPaneSettings(): IPropertyPaneSettings {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
